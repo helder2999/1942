dfe86dafa8232fe966306e45ff497a025e3815bd
helder2999 <helder2999@gmail.com>
2020-03-12 00:35:05 -0100

    Add files via upload

---
+++ precache-manifest.5e4b72f194aaf2f0a4d93854da9108ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2566d7149170b452ad",
+    "url": "/css/app.cefb6f8c.css"
+  },
+  {
+    "revision": "d34c108da4569757d03d",
+    "url": "/css/chunk-vendors.6fafcc51.css"
+  },
+  {
+    "revision": "ee3a4b51cf1e427bb31d50a93ae48e95",
+    "url": "/img/PRJ1984.ee3a4b51.png"
+  },
+  {
+    "revision": "8b287f43cba3308f270681da35f34734",
+    "url": "/img/megaphone.8b287f43.png"
+  },
+  {
+    "revision": "08a34cedd7ac25535144844c1442ebef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ddf9362834abf54173e",
+    "url": "/js/about.d08fd8c6.js"
+  },
+  {
+    "revision": "0c2566d7149170b452ad",
+    "url": "/js/app.52f00c06.js"
+  },
+  {
+    "revision": "d34c108da4569757d03d",
+    "url": "/js/chunk-vendors.073bb302.js"
+  },
+  {
+    "revision": "80de6de119d4dfa3054e437899c63a5a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
